5680e795f1e5347735ef593626b4adb276c028e9
Jamison Blackwell <blackwellj1040@gmail.com>
2019-09-13 22:04:51 -0400

    last push

--- api/server.spec.js
+++ api/server.spec.js
@@ -40,6 +40,41 @@ describe('server', ()=> {
             })
         });
 
+        describe('POST /register', () => {
+            beforeEach(async () => {
+                await db('users').truncate(); // guarantees table is cleaned out before any test run
+            })
+                it('should return 1 for body', () => {
+                    return request(server)
+                    .post('/api/auth/register')
+                    .send({
+                        username: 'jffck',
+                        password: 'ygdw'
+                    })
+                    .then(res => {
+                        expect(res.body.length).toBe(0);
+                    })
+                    })
+            
+           
+            });
+    
+        describe('POST /login', async () => {
+            it('should return status 201', () => {
+                return request(server)
+                .post('/api/auth/login')
+                .send({
+                    username: 'jafffdfck',
+                    password: 'yfefdgfp'
+                })
+                .then(res => {
+                    expect(res.status).toBe(201);
+                })
+                })
+            });
+
         
+
+
    
     
