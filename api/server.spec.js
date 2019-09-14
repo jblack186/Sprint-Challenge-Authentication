a92f108f41af27ea8b5d7e6c00f23b08306b65dd
Jamison Blackwell <blackwellj1040@gmail.com>
2019-09-13 21:57:51 -0400

    finished mvp

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,14 +1,45 @@
 const request = require('supertest');
 const server = require('./server.js');
+const db = require('../database/dbConfig.js');
 
-describe('the register', () => {
+describe('server', ()=> {
+    it('test is running', () => {
+        expect(process.env.DB_ENV). toBe('testing');
+    })
+})
     describe('POST /register', () => {
+        beforeEach(async () => {
+            await db('users').truncate(); // guarantees table is cleaned out before any test run
+        })
+            it('should return 1 for body', () => {
+                return request(server)
+                .post('/api/auth/register')
+                .send({
+                    username: 'jffck',
+                    password: 'ygdw'
+                })
+                .then(res => {
+                    expect(res.body.length).toBe(0);
+                })
+                })
+        
+       
+        });
+
+    describe('POST /register', async () => {
         it('should return status 201', () => {
             return request(server)
-            .post('/register')
+            .post('/api/auth/register')
+            .send({
+                username: 'jafffdfck',
+                password: 'yfefdgfp'
+            })
             .then(res => {
                 expect(res.status).toBe(201);
             })
+            })
         });
-    })
-})
+
+        
+   
+    
